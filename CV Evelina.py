8bf9cac2f7bd333221c5157cc341e455c6aad395
lupanevelina <70973228+lupanevelina@users.noreply.github.com>
2020-09-15 21:49:39 +0300

    Add files via upload

---
+++ CV Evelina.py
@@ -0,0 +1,11 @@
+print("Lupan Evelina")
+print("09.11.2004")
+print("or.Balti,Str Conev38 ap65")
+print("078737517")
+print("lupanevelina@mail.ru")
+print("I.P.L.T.'M.Eminescu' ")
+print("eleva in clasa 10 D")
+print("studii gimnaziale")
+
+
+
